0675082a3e49c95fd1138af30f19d76074e0aba4
Aditya Jadaun <aadijaadaun@gmail.com>
2019-07-17 11:13:14 +0000

    Switch to Illusion Kernel Thanks to @anirudhgupta109 for Illusion Kernel
    Signed-off-by: Aditya Jadaun <aadijaadaun@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@ DEVICE_PATH := device/oneplus/enchilada
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
 # Kernel
-TARGET_KERNEL_CONFIG := redflare_defconfig
+TARGET_KERNEL_CONFIG := kronic_defconfig
 
 # inherit from the proprietary version
 -include vendor/oneplus/enchilada/BoardConfigVendor.mk
