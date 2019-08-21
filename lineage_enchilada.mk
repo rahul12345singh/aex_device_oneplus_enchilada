LineageOS/android_device_oneplus_enchilada

Revert "enchilada: Don't set PRODUCT_MODEL"
This reverts commit e3f5c9daf3912f4a7eaf849c8970bdaeff14090c.

Change-Id: I94c684b34a4c2951aea4e05b50ff1561ea7ff0ff

lineage_enchilada.mk
@@ -28,6 +28,7 @@ PRODUCT_NAME := lineage_enchilada
 PRODUCT_DEVICE := enchilada
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := ONEPLUS A6003
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
@@ -38,8 +39,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=OnePlus6 \
     PRODUCT_NAME=OnePlus6 \
     PRIVATE_BUILD_DESC="OnePlus6-user 8.1.0 OPM1.171019.011 273 release-keys"
-
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
-    ro.product.model
 
 BUILD_FINGERPRINT := OnePlus/OnePlus6/OnePlus6:8.1.0/OPM1.171019.011/06140300:user/release-keys

rahul12345singh/aex_device_oneplus_enchilada

Signed-off-by: Aditya Jadaun <aadijaadaun@gmail.com>

lineage_enchilada.mk
@@ -28,6 +28,7 @@ PRODUCT_NAME := lineage_enchilada
 PRODUCT_DEVICE := enchilada
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := ONEPLUS A6000
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
@@ -38,8 +39,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=OnePlus6 \
     PRODUCT_NAME=OnePlus6 \
     PRIVATE_BUILD_DESC="OnePlus6-user 8.1.0 OPM1.171019.011 273 release-keys"
-
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
-    ro.product.model
 
 BUILD_FINGERPRINT := OnePlus/OnePlus6/OnePlus6:8.1.0/OPM1.171019.011/06140300:user/release-keys